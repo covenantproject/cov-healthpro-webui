c03ba28e05dfe8d212f619bef13f29a4c707ed66
Ali Momeni <a9tfczz@mmm.com>
2020-04-12 14:43:35 -0600

    initial commit

---
+++ covidAdminInterface/src/data/data.js
@@ -0,0 +1,92 @@
+const data = {
+  users: [
+    {
+      firstName: John,
+      lastName: Doe,
+      lastReported: 1586721800,
+      duration: 9000,
+      distance: 1225.5
+    },
+    {
+      firstName: John,
+      lastName: Doe,
+      lastReported: 1586721800,
+      duration: 9000,
+      distance: 1225.5
+    },
+    {
+      firstName: John,
+      lastName: Doe,
+      lastReported: 1586721800,
+      duration: 9000,
+      distance: 1225.5
+    },
+    {
+      firstName: John,
+      lastName: Doe,
+      lastReported: 1586721800,
+      duration: 9000,
+      distance: 1225.5
+    },
+    {
+      firstName: John,
+      lastName: Doe,
+      lastReported: 1586721800,
+      duration: 9000,
+      distance: 1225.5
+    },
+  ],
+  medicalRequest: [
+    {
+      firstName: John,
+      lastName: Doe,
+      reported: 1586721800,
+      message: "User message goes here"
+    },
+    {
+      firstName: John,
+      lastName: Doe,
+      reported: 1586721800,
+      message: "User message goes here"
+    },
+    {
+      firstName: John,
+      lastName: Doe,
+      reported: 1586721800,
+      message: "User message goes here"
+    },
+    {
+      firstName: John,
+      lastName: Doe,
+      reported: 1586721800,
+      message: "User message goes here"
+    },
+    {
+      firstName: John,
+      lastName: Doe,
+      reported: 1586721800,
+      message: "User message goes here"
+    },
+  ],
+  foodRequest: [
+    {
+      firstName: John,
+      lastName: Doe,
+      reported: 1586721800,
+      message: "User message goes here"
+    },
+    {
+      firstName: John,
+      lastName: Doe,
+      reported: 1586721800,
+      message: "User message goes here"
+    },
+    {
+      firstName: John,
+      lastName: Doe,
+      reported: 1586721800,
+      message: "User message goes here"
+    },
+  ]
+}
+export default data
