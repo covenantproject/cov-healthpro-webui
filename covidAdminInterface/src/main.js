a61c84f06fb6c011322cca004dbdf640bb87c405
Ali Momeni <a9tfczz@mmm.com>
2020-04-12 18:16:18 -0600

    changes to package.json

--- covidAdminInterface/src/main.js
+++ covidAdminInterface/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from 'vue-router'
+
+Vue.use(VueRouter)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  render: h => h(App),
 }).$mount("#app");
