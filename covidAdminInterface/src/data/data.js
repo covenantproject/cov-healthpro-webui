56fab12272b60898bb5de223661c7301fd7586c4
Rey Johnson <wjohnson5@mmm.com>
2020-04-14 20:34:32 -0600

    Add sample data for patient information page

--- covidAdminInterface/src/data/data.js
+++ covidAdminInterface/src/data/data.js
@@ -129,7 +129,21 @@ const data = {
       age: "22yr",
       address: "1234 Address St, City, Province, Country"
     }
-  ]
+  ],
+  userProfile: {
+    firstName: "John",
+    lastName: "Doe",
+    age: 22,
+    sex: "Male",
+    patientType: "Medical Quarantine",
+    quarantineStart: 1586917814,
+    quarantineDuration: 1209600,
+    quarantineLatitude: -8.784600,
+    quarantineLongitude: 115.159735,
+    covidStatus: "Exposed",
+    quarantineStatus: "Compliant",
+    contactInfo: "555-555-5555 (WhatsApp)"
+  }
 };
 
 export default data;
