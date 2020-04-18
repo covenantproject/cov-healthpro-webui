ac14390d6b46e30941cef6feecc099fbf5c38159
Ali Momeni <a9tfczz@mmm.com>
2020-04-18 16:45:42 -0600

    axios added

--- covidAdminInterface/src/main.js
+++ covidAdminInterface/src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/index';
 import './../node_modules/bulma/css/bulma.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
 // import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 // import 'leaflet/dist/leaflet.css';
 //
