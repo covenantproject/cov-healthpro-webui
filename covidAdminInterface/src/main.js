1d181d034c9426f86bd5c40fa8e9e195f9b5b57f
Ali Momeni <a9tfczz@mmm.com>
2020-04-18 16:57:48 -0600

    vuex store added, sample store added, axios added

--- covidAdminInterface/src/main.js
+++ covidAdminInterface/src/main.js
@@ -4,8 +4,11 @@ import router from './router/index';
 import './../node_modules/bulma/css/bulma.css';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import Vuex from 'vuex';
+
 
 Vue.use(VueAxios, axios);
+Vue.use(Vuex);
 // import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 // import 'leaflet/dist/leaflet.css';
 //
