ddf9a664a64b8322361c47803b4442ded6d069b4
Ali Momeni <a9tfczz@mmm.com>
2020-05-01 17:48:22 -0600

    registration more form items

--- covenant/src/components/registrationComponents/registrationComponent.vue
+++ covenant/src/components/registrationComponents/registrationComponent.vue
@@ -72,55 +72,29 @@
             <date-picker v-model="time1" valueType="format"></date-picker>
         </div>
 
-
         <div class="field">
-            <label class="label">Email</label>
-            <div class="control has-icons-left has-icons-right">
-                <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
-                <span class="icon is-small is-left">
-      <i class="fas fa-envelope"></i>
-    </span>
-            </div>
-            <p class="help is-danger">This email is invalid</p>
-        </div>
-
-
-        <div class="field">
-            <label class="label">Message</label>
+            <label class="label">Phone Number</label>
             <div class="control">
-                <textarea class="textarea" placeholder="Textarea"></textarea>
+                <input class="input" type="text" placeholder="Phone Number">
             </div>
         </div>
 
+        <p></p>
+
         <div class="field">
+            <label class="label">User Roles</label>
             <div class="control">
                 <label class="checkbox">
                     <input type="checkbox">
-                    I agree to the <a href="#">terms and conditions</a>
+                    Health Professional
+                </label>
+                <br>
+                <label class="checkbox">
+                    <input type="checkbox">
+                    User Manager (can add new users)
                 </label>
             </div>
-        </div>
 
-        <div class="field">
-            <div class="control">
-                <label class="radio">
-                    <input type="radio" name="question">
-                    Yes
-                </label>
-                <label class="radio">
-                    <input type="radio" name="question">
-                    No
-                </label>
-            </div>
-        </div>
-
-        <div class="field is-grouped">
-            <div class="control">
-                <button class="button is-link">Submit</button>
-            </div>
-            <div class="control">
-                <button class="button is-link is-light">Cancel</button>
-            </div>
         </div>
     </div>
 </template>
