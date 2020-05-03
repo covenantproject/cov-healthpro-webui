38447db59ca80ba7f9338577f3da4437d505526d
Ali Momeni <a9tfczz@mmm.com>
2020-05-03 12:09:46 -0600

    saving progress

--- covenant/src/App.vue
+++ covenant/src/App.vue
@@ -4,17 +4,11 @@
             <div class="container">
                 <div class="navbar-brand">
                     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true">Hey</span>
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                     </a>
                 </div>
-
-                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
 
                 <div id="navbarBasicExample" class="navbar-menu">
                     <div class="navbar-start">
