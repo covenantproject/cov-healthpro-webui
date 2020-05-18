6a0352ad96f6e5fd02e75c0cf81fa3c89a0b4182
Rey Johnson <wjohnson5@mmm.com>
2020-05-17 22:51:08 -0600

    Split search bar into multiple inputs

--- covenant/src/components/searchComponents/SearchBarComponent.vue
+++ covenant/src/components/searchComponents/SearchBarComponent.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="container" id="main">
         <div class="columns is-mobile is-centered">
-            <div class="column is-three-fifths">
-                <label>
-                    <input id="search-box" class="input" type="text" placeholder="Search User">
-                </label>
+            <div class="column">
+                <label class="label">First Name</label>
+                <input id="first-name" class="input" type="text" placeholder="First Name">
+            </div>
+            <div class="column">
+                <label class="label">Last Name</label>
+                <input id="last-name" class="input" type="text" placeholder="Last Name">
+            </div>
+            <div class="column">
+                <label class="label">Phone Number</label>
+                <input id="phone-number" class="input" type="text" placeholder="Phone Number">
             </div>
             <div class="column is-one-fifth">
                 <button v-bind:class="[searchIsLoading ? 'is-loading' : '', 'button', 'is-info']" type="submit" @click="onSearchButtonClicked">Search
